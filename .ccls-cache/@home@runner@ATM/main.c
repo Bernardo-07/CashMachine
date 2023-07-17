8086f5a13ffcc4446496778fc3a75ac928130d53
Bernardo-07 <84275344+Bernardo-07@users.noreply.github.com>
2023-07-17 22:42:09 +0000

    Múltiplos de 5

--- .ccls-cache/@home@runner@ATM/main.c
+++ .ccls-cache/@home@runner@ATM/main.c
@@ -9,7 +9,9 @@ int main() {
 
   n100 = money/100;
   n50 = (money%100)/50;
+  n10 = (money%50)/10;
+  n5 = (money%10)/5;
 
-  printf("\n%d notas de 100\n%d notas de 50",n100,n50);
+  printf("\n%d notas de 100\n%d notas de 50\n%d notas de 10\n%d notas de 5", n100, n50, n10, n5);
   return 0;
 }
