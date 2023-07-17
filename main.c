cc31b07c56bbfaa15929fbd9926e1dc7f14b9304
Bernardo-07 <84275344+Bernardo-07@users.noreply.github.com>
2023-07-17 22:38:56 +0000

    Update

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main(void) {
-  printf("Hello World\n");
+int main() {
+  int n2, n5, n10, n20, n50, n100;
+  int money;
+  
+  printf("Quanto você deseja sacar?\n");
+  scanf("%d",&money);
+
+  n100 = money/100;
+  n50 = (money%100)/50;
+
+  printf("\n%d notas de 100\n%d notas de 50",n100,n50);
   return 0;
 }
